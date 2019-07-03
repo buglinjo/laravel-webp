3841a02ab9fa8fcd1a03122ca7d8dcd4472cd31c
Irakli Tchitadze <irakli.tchitadze@gmail.com>
2019-07-03 01:52:58 -0400

    Working v2.0 with cwebp

--- src/Webp.php
+++ src/Webp.php
@@ -2,81 +2,27 @@
 
 namespace Buglinjo\LaravelWebp;
 
+use Exception;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\File;
+use Illuminate\Http\UploadedFile;
 
 class Webp
 {
     /**
-     * @var string
+     * @param UploadedFile $image
+     * @return Cwebp|Traits\WebpTrait
+     * @throws Exception
      */
-    private $cwebpPath;
-
-    /**
-     * @var string
-     */
-    private $inputPath;
-
-    /**
-     * @var int
-     */
-    private $quality;
-
-    /**
-     * LaravelWebp constructor.
-     */
-    public function __construct()
+    public static function make(UploadedFile $image)
     {
-        $this->cwebpPath = Config::get('laravel-webp.cwebp_path');
-        $this->quality = Config::get('laravel-webp.default_quality');
-    }
+        $driver = Config::get('laravel-webp.default_driver');
 
-    /**
-     * @param $inputPath
-     * @return $this
-     */
-    public function makeWebp($inputPath): self
-    {
-        $this->inputPath = $inputPath;
-
-        return $this;
-    }
-
-    /**
-     * @param $quality
-     * @return $this
-     */
-    public function quality($quality): self
-    {
-        $this->quality = $quality;
-
-        return $this;
-    }
-
-    /**
-     * @param      $outputPath
-     * @param null $quality
-     * @return bool
-     */
-    public function save($outputPath, $quality = null): bool
-    {
-        $thisQuality = $quality ? $quality : $this->quality;
-
-        shell_exec($this->cwebpPath . ' -q ' . $thisQuality . ' ' . $this->inputPath . ' -o ' . $outputPath);
-
-        if (File::exists($outputPath)) {
-            return true;
-        } else {
-            return false;
+        if ($driver === 'php-gd') {
+            //
+        } elseif ($driver === 'cwebp') {
+            return (new Cwebp())->make($image);
         }
-    }
 
-    /**
-     * @param string $inputPath
-     * @return self
-     */
-    public static function make($inputPath): self
-    {
-        return (new self())->makeWebp($inputPath);
+        throw new Exception('Driver [' . $driver . '] is not supported.');
     }
 }
