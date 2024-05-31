9658827a2b16e243e7dadb12815d450c664e2403
Terax <19204073+terax6669@users.noreply.github.com>
2024-05-31 13:29:29 +0200

    Fix argument type exception in cwebp

--- src/Exceptions/CwebpShellExecutionFailed.php
+++ src/Exceptions/CwebpShellExecutionFailed.php
@@ -7,25 +7,25 @@ use Throwable;
 
 class CwebpShellExecutionFailed extends Exception
 {
-    public function __construct(string $cmd, $output, $code = 0, Throwable $previous = null)
+    public function __construct(string $cmd, string $output, $code = 0, Throwable $previous = null)
     {
         parent::__construct($this->makeMessage($cmd, $code, $output), $code, $previous);
     }
 
     /**
      * @param string $cmd
-     * @param int $exitCode
-     * @param $output
+     * @param int|null $exitCode
+     * @param string $output
      * @return string
      */
-    protected function makeMessage(string $cmd, int $exitCode, $output): string
+    protected function makeMessage(string $cmd, ?int $exitCode, string $output): string
     {
         return 'Image conversion to WebP using cwebp failed with error code ' . $exitCode . ".\n"
             . "This command was used to execute cwebp: \n"
             . "  " . $cmd . "\n"
             . (
-            count($output)
-                ? "The following output was sent to stdout: \n  " . join("\n  ", $output)
+            trim($output)
+                ? "The following output was sent to stdout: \n$output"
                 : "No output was sent to stdout"
             );
     }
